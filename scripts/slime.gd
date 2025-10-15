5bee9f1089db4ba0cb798dec127684d402954cd7
UniquityLostBraincells <uniquitylostbraincells@gmail.com>
2025-10-15 20:22:07 +0500

    Add slime enemy

---
+++ scripts/slime.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+var speed = 50
+var player_chase = false
+var player = null
+
+func _physics_process(_delta):
+	if player_chase:
+		position += (player.position -position)/speed
+		#since speed is being divided, greater speed variable = slower movement
+		#play walk animation when moving
+		$AnimatedSprite2D.play("walk")
+		
+		#flip sprite if moving left
+		if (player.position.x - position.x) < 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = true
+	
+	else:
+		$AnimatedSprite2D.play("idle")
+
+
+func _on_detection_area_body_entered(body: CharacterBody2D) -> void:
+	player = body
+	player_chase = true
+
+
+
+func _on_detection_area_body_exited(_body: CharacterBody2D) -> void:
+	player = null
+	player_chase = false
