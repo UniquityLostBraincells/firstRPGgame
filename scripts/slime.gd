c9d60ce676ca0f8c6b1c9f8a5d54a382a3e5a57c
UniquityLostBraincells <uniquitylostbraincells@gmail.com>
2025-10-17 11:37:11 +0500

    Add slime spawn animation

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -2,22 +2,29 @@ extends CharacterBody2D
 var speed = 50
 var player_chase = false
 var player = null
+var spawn_ani_finished = false
+
+func _ready() -> void:
+	$AnimatedSprite2D.play("spawn")
+func _on_idle_timeout_timeout() -> void:
+	spawn_ani_finished = true
 
 func _physics_process(_delta):
-	if player_chase:
-		position += (player.position -position)/speed
-		#since speed is being divided, greater speed variable = slower movement
-		#play walk animation when moving
-		$AnimatedSprite2D.play("walk")
+	if spawn_ani_finished:
+		if player_chase:
+			position += (player.position - position)/speed
+			#since speed is being divided, greater speed variable = slower movement
+			#play walk animation when moving
+			$AnimatedSprite2D.play("walk")
 		
 		#flip sprite if moving left
-		if (player.position.x - position.x) < 0:
-			$AnimatedSprite2D.flip_h = true
-		else:
-			$AnimatedSprite2D.flip_h = false
+			if (player.position.x - position.x) < 0:
+				$AnimatedSprite2D.flip_h = true
+			else:
+				$AnimatedSprite2D.flip_h = false
 	
-	else:
-		$AnimatedSprite2D.play("idle")
+		else:
+			$AnimatedSprite2D.play("idle")
 
 
 func _on_detection_area_body_entered(body: CharacterBody2D) -> void:
